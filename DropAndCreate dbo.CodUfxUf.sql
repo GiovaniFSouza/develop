43b6e9b88a99916b090b4e9280a8e39891582fd1
Giovani Faria de Souza <gci1@hotmail.com>
2019-11-13 14:55:11 -0200

    Acrescido campo sigla na consulta codufxufs

---
+++ DropAndCreate dbo.CodUfxUf.sql
@@ -0,0 +1,19 @@
+﻿USE [PfjDB]
+GO
+
+/****** Object: View [dbo].[CodUfxUf] Script Date: 13/11/2019 14:50:47 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP VIEW [dbo].[CodUfxUf];
+
+
+GO
+CREATE VIEW [dbo].[CodUfxUf]
+	AS SELECT cd.Id , uf.nome , cd.sigla
+	--, cd.sigla, uf.codigo as siglas
+	FROM [CODUF] as cd								 
+	inner join [Ufs] as uf (NOLOCK) on uf.codigo = cd.sigla
--- WebApi.Cliente/Models/CodUfxUfs.cs
+++ WebApi.Cliente/Models/CodUfxUfs.cs
@@ -12,6 +12,9 @@ namespace WebApi.Cliente.Models
         
         public int Id { get; set; }
         public string nome { get; set; }
+#pragma warning disable IDE1006 // Naming Styles
+        public string sigla { get; set; }
+#pragma warning restore IDE1006 // Naming Styles
 
     }
 }
